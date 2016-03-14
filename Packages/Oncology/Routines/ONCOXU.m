d1837c68397e0f5530ce053734c8ac33581e30d9
Joseph Snyder <joe.snyder@kitware.com>
2016-03-14 14:10:33 -0400

    Generate OSEHRA VistA through 3/14/2016
    Generate an example branch of OSEHRA-VistA by adding the OSEHRA Core
    submissions to an instance built with the OSEHRA/VistA-M master code
    
    Patch the system using the PatchSequenceApply script on the available
    patches from the foia-vista.osehra.org files for November 2015 - March
    2016.

--- Packages/Oncology/Routines/ONCOXU.m
+++ Packages/Oncology/Routines/ONCOXU.m
@@ -1,11 +1,11 @@
-ONCOXU ; GENERATED FROM 'ONCO XABSTRACT RECORD' PRINT TEMPLATE (#826) ; 09/27/12 ; (FILE 165.5, MARGIN=80)
+ONCOXU ; GENERATED FROM 'ONCO XABSTRACT RECORD' PRINT TEMPLATE (#826) ; 03/14/16 ; (FILE 165.5, MARGIN=80)
  G BEGIN
 N W !
 T W:$X ! I '$D(DIOT(2)),DN,$D(IOSL),$S('$D(DIWF):1,$P(DIWF,"B",2):$P(DIWF,"B",2),1:1)+$Y'<IOSL,$D(^UTILITY($J,1))#2,^(1)?1U1P1E.E X ^(1)
  S DISTP=DISTP+1,DILCT=DILCT+1 D:'(DISTP#100) CSTP^DIO2
  Q
 DT I $G(DUZ("LANG"))>1,Y W $$OUT^DIALOGU(Y,"DD") Q
- I Y W $P("JAN^FEB^MAR^APR^MAY^JUN^JUL^AUG^SEP^OCT^NOV^DEC",U,$E(Y,4,5))_" " W:Y#100 $J(Y#100\1,2)_"," W Y\10000+1700 W:Y#1 "  "_$E(Y_0,9,10)_":"_$E(Y_"000",11,12) Q
+ X ^DD("DD")
  W Y Q
 M D @DIXX
  Q
@@ -18,7 +18,7 @@ BEGIN ;
  S X="PATIENT SUMMARY",X=$J("",$S($D(DIWR)+$D(DIWL)=2:DIWR-DIWL+1,$D(IOM):IOM,1:80)-$L(X)\2-$X)_X K DIP K:DN Y W X
  D N:$X>67 Q:'DN  W ?67 N %I,%H,% D NOW^%DTC S Y=X K DIP K:DN Y S Y=X D DT
  D T Q:'DN  W ?2 W !,"" K DIP K:DN Y
- S I(100)="^ONCO(160,",J(100)=160 S I(0,0)=D0 S DIP(1)=$S($D(^ONCO(165.5,D0,0)):^(0),1:"") S X=$P(DIP(1),U,2),X=X S D(0)=+X S D0=D(0) I D0>0 D A1
+ S I(100)="^ONCO(160,",J(100)=160 S I(0,0)=D0 S DIP(1)=$S($D(^ONCO(165.5,D0,0)):^(0),1:"") S X=$P(DIP(1),U,2),X=X  S D(0)=+X S D0=D(0) I D0>0 D A1
  G A1R
 A1 ;
  D N:$X>2 Q:'DN  W ?2 W "Name: "
@@ -26,13 +26,13 @@ A1 ;
  D N:$X>39 Q:'DN  W ?39 W "SSN: "
  D N:$X>46 Q:'DN  W ?46 D SSN^ONCOES W $J(X,12) K Y(160,2)
  D N:$X>2 Q:'DN  W ?2 W "Sex: "
- S X=$G(^ONCO(160,D0,0)) D N:$X>8 Q:'DN  W ?8 S Y=$P(X,U,8) W:Y]"" $S($D(DXS(1,Y)):DXS(1,Y),1:Y)
+ S X=$G(^ONCO(160,D0,0)) D N:$X>8 Q:'DN  W ?8 S Y=$P(X,U,8) W:Y]"" $E($$SET^DIQ(160,10,Y),1,25)
  D N:$X>39 Q:'DN  W ?39 W "DOB: "
  D N:$X>46 Q:'DN  W ?46 D DOB^ONCOES W $J(X,11) K Y(160,3)
  D N:$X>2 Q:'DN  W ?2 W "Race: "
  S X=$G(^ONCO(160,D0,0)) D N:$X>8 Q:'DN  W ?8 S Y=$P(X,U,6) S Y=$S(Y="":Y,$D(^ONCO(164.46,Y,0))#2:$P(^(0),U),1:Y) W $E(Y,1,50)
  D N:$X>39 Q:'DN  W ?39 W "Status: "
- S X=$G(^ONCO(160,D0,1)) D N:$X>47 Q:'DN  W ?47 S Y=$P(X,U,1) W:Y]"" $S($D(DXS(2,Y)):DXS(2,Y),1:Y)
+ S X=$G(^ONCO(160,D0,1)) D N:$X>47 Q:'DN  W ?47 S Y=$P(X,U,1) W:Y]"" $E($$SET^DIQ(160,15,Y),1,5)
  W ?47 W !," " K DIP K:DN Y
  W ?58 D DLC^ONCOCRF,DATEOT^ONCOES K DIP K:DN Y
  D N:$X>2 Q:'DN  W ?2 W "Date of Last Contact or Death:"
@@ -42,13 +42,13 @@ A1 ;
  D N:$X>2 Q:'DN  W ?2 W "Autopsy #: "
  S X=$G(^ONCO(160,D0,1)) D N:$X>33 Q:'DN  W ?33,$E($P(X,U,10),1,15)
  D N:$X>2 Q:'DN  W ?2 W "Cause of Death: "
- D N:$X>33 Q:'DN  W ?33 S Y=$P(X,U,3) S Y(0)=Y I Y'="" N ONCICD,CODE,SPACE S ONCICD=$$ICDDX^ICDCODE(Y) S:(ONCICD=-1) Y=-1 S:(Y'=-1) CODE=$P(ONCICD,U,2),SPACE=$S($L(CODE)=4:"   ",$L(CODE)=5:"  ",1:" "),Y=CODE_SPACE_$P(ONCICD,U,4) W $E(Y,1,30)
+ D N:$X>33 Q:'DN  W ?33 S Y=$P(X,U,3) S Y(0)=Y S X=Y D SDOT^ONCFUNC W $E(Y,1,30)
  Q
 A1R ;
  K J(100),I(100) S:$D(I(0,0)) D0=I(0,0)
  W ?33 W !!," " K DIP K:DN Y
  D N:$X>2 Q:'DN  W ?2 W "Abstract Status: "
- S X=$G(^ONCO(165.5,D0,7)) S Y=$P(X,U,2) W:Y]"" $S($D(DXS(3,Y)):DXS(3,Y),1:Y)
+ S X=$G(^ONCO(165.5,D0,7)) S Y=$P(X,U,2) W:Y]"" $E($$SET^DIQ(165.5,91,Y),1,14)
  D N:$X>39 Q:'DN  W ?39 W "Date Case Completed: "
  S Y=$P(X,U,1) S Y(0)=Y S X=Y D DATEOT^ONCOES W $E(Y,1,30)
  D N:$X>2 Q:'DN  W ?2 W "Acc/Seq Number: "
--- Packages/Oncology/Routines/ONCOY58.m
+++ Packages/Oncology/Routines/ONCOY58.m
@@ -1,11 +1,11 @@
-ONCOY58 ; GENERATED FROM 'ONCOY58' PRINT TEMPLATE (#816) ; 09/27/12 ; (FILE 165.5, MARGIN=80)
+ONCOY58 ; GENERATED FROM 'ONCOY58' PRINT TEMPLATE (#816) ; 03/14/16 ; (FILE 165.5, MARGIN=80)
  G BEGIN
 N W !
 T W:$X ! I '$D(DIOT(2)),DN,$D(IOSL),$S('$D(DIWF):1,$P(DIWF,"B",2):$P(DIWF,"B",2),1:1)+$Y'<IOSL,$D(^UTILITY($J,1))#2,^(1)?1U1P1E.E X ^(1)
  S DISTP=DISTP+1,DILCT=DILCT+1 D:'(DISTP#100) CSTP^DIO2
  Q
 DT I $G(DUZ("LANG"))>1,Y W $$OUT^DIALOGU(Y,"DD") Q
- I Y W $P("JAN^FEB^MAR^APR^MAY^JUN^JUL^AUG^SEP^OCT^NOV^DEC",U,$E(Y,4,5))_" " W:Y#100 $J(Y#100\1,2)_"," W Y\10000+1700 W:Y#1 "  "_$E(Y_0,9,10)_":"_$E(Y_"000",11,12) Q
+ X ^DD("DD")
  W Y Q
 M D @DIXX
  Q
@@ -16,20 +16,20 @@ BEGIN ;
  D T Q:'DN  D N W ?0 W "* FOLLOW-UP *"
  W ?15 K FUQA K DIP K:DN Y
  D T Q:'DN  W ?0 S X="" D FHP^ONCODLF W $E(X,1,80) K Y(165.5,150)
- S I(100)="^ONCO(160,",J(100)=160 S I(0,0)=D0 S DIP(1)=$S($D(^ONCO(165.5,D0,0)):^(0),1:"") S X=$P(DIP(1),U,2),X=X S D(0)=+X S D0=D(0) I D0>0 D A1
+ S I(100)="^ONCO(160,",J(100)=160 S I(0,0)=D0 S DIP(1)=$S($D(^ONCO(165.5,D0,0)):^(0),1:"") S X=$P(DIP(1),U,2),X=X  S D(0)=+X S D0=D(0) I D0>0 D A1
  G A1R
 A1 ;
  D T Q:'DN  D N D N:$X>2 Q:'DN  W ?2 W "Status........................:"
- S X=$G(^ONCO(160,D0,1)) D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,1) W:Y]"" $S($D(DXS(1,Y)):DXS(1,Y),1:Y)
+ S X=$G(^ONCO(160,D0,1)) D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,1) W:Y]"" $E($$SET^DIQ(160,15,Y),1,5)
  W ?41 I $P($G(^ONCO(160,D0,1)),U,1)=1 G A1R^ONCOY58 K DIP K:DN Y
  D N:$X>2 Q:'DN  W ?2 W "Cause of Death................:"
- S X=$G(^ONCO(160,D0,1)) D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,3) S Y(0)=Y I Y'="" N ONCICD,CODE,SPACE S ONCICD=$$ICDDX^ICDCODE(Y) S:(ONCICD=-1) Y=-1 S:(Y'=-1) CODE=$P(ONCICD,U,2),SPACE=$S($L(CODE)=4:"   ",$L(CODE)=5:"  ",1:" "),Y=CODE_SPACE_$P(ONCICD,U,4) W $E(Y,1,30)
+ S X=$G(^ONCO(160,D0,1)) D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,3) S Y(0)=Y S X=Y D SDOT^ONCFUNC W $E(Y,1,30)
  D N:$X>2 Q:'DN  W ?2 W "ICD Revision..................:"
- S X=$G(^ONCO(160,D0,1)) D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,4) W:Y]"" $S($D(DXS(2,Y)):DXS(2,Y),1:Y)
+ S X=$G(^ONCO(160,D0,1)) D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,4) W:Y]"" $E($$SET^DIQ(160,20,Y),1,31)
  D N:$X>2 Q:'DN  W ?2 W "State Death Cert..............:"
- D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,14) W:Y]"" $S($D(DXS(3,Y)):DXS(3,Y),1:Y)
+ D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,14) W:Y]"" $E($$SET^DIQ(160,19.1,Y),1,45)
  D N:$X>2 Q:'DN  W ?2 W "Autopsy.......................:"
- D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,13) W:Y]"" $S($D(DXS(4,Y)):DXS(4,Y),1:Y)
+ D N:$X>34 Q:'DN  W ?34 S Y=$P(X,U,13) W:Y]"" $E($$SET^DIQ(160,22.9,Y),1,45)
  Q
 A1R ;
  K J(100),I(100) S:$D(I(0,0)) D0=I(0,0)
