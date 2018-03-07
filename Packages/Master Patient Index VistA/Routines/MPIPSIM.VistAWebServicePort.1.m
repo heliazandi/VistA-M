88439806557f4e10e6b6865e0fb51afa8101bf28
US DVA <va.gov>
2018-03-07 09:46:15 -0500

    Import FOIA Release from March 2018

---
+++ Packages/Master Patient Index VistA/Routines/MPIPSIM.VistAWebServicePort.1.m
@@ -0,0 +1,6 @@
+ ;MPIPSIM.VistAWebServicePort.1
+ ;(C)InterSystems, generated for class MPIPSIM.VistAWebServicePort.  Do NOT edit. 12/20/2016 03:08:25PM
+ ;;5A537A6B;MPIPSIM.VistAWebServicePort
+ ;
+zexecute(requestXML) public {
+ Quit ..WebMethod("execute").Invoke($this,"",.requestXML) }
